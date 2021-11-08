2ae8ce84a3095e7eaf7ae1e5474e4bad8a8043b3
pkdev08 <54294685+pkdev08@users.noreply.github.com>
2021-11-08 01:01:02 -0500

    fix runin

--- src/inhibitors/runIn.js
+++ src/inhibitors/runIn.js
@@ -1,11 +1,17 @@
 const { Inhibitor } = require('klasa');
+const friendlyrunIn = {
+	GUILD_TEXT: "text",
+	GUILD_NEWS: 'news',
+	DM: 'dm',
+	GUILD_ANY: 'guild any'
+}
 
 module.exports = class extends Inhibitor {
 
 	run(message, command) {
 		if (!command.runIn.length) throw message.language.get('INHIBITOR_RUNIN_NONE', command.name);
 		if (command.runIn.includes('GUILD_ANY') && message.guild) return;
-		if (!command.runIn.includes(message.channel.type)) throw message.language.get('INHIBITOR_RUNIN', command.runIn.join(', '));
+		if (!command.runIn.includes(message.channel.type)) throw message.language.get('INHIBITOR_RUNIN', command.runIn.map(key => friendlyrunIn[key]).join(', '));
 	}
 
 };
